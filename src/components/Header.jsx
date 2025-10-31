56c055cf6d9311bfa7b3c199f93184b77d4012dc
Fábio Santos <fabiospinheira@gmail.com>
2025-10-31 17:21:16 -0300

    fix(header): header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,37 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import { FaUser, FaImages, FaRunning, FaAppStore } from "react-icons/fa";
 
 export default function Header({ profilePic }) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/`);
-  };
-
   return (
     <header className="header">
+      {/* Navegação em desktop */}
       <nav className="header-nav pc">
         <img src={profilePic} alt="Foto de perfil" className="headerPic" />
 
-        <a href="/">Sobre</a>
-        <a href="/WIP">Galeria</a>
-        <a href="/WIP">Atividades</a>
-        <a href="/WIP">Komorebi</a>
+        <Link to="/">Sobre</Link>
+        <Link to="/WIP">Galeria</Link>
+        <Link to="/WIP">Atividades</Link>
+        <Link to="/WIP">Komorebi</Link>
       </nav>
 
+      {/* Navegação em mobile (ícones) */}
       <nav className="header-nav mobile">
         <img src={profilePic} alt="Foto de perfil" className="headerPic" />
 
-        <a href="#about" title="Sobre">
+        <Link to="/" title="Sobre">
           <FaUser size={24} color="#fff" />
-        </a>
-        <a href="#gallery" title="Galeria">
+        </Link>
+        <Link to="/WIP" title="Galeria">
           <FaImages size={24} color="#fff" />
-        </a>
-        <a href="#activities" title="Atividades">
+        </Link>
+        <Link to="/WIP" title="Atividades">
           <FaRunning size={24} color="#fff" />
-        </a>
-        <a href="#Komorebi" title="App">
+        </Link>
+        <Link to="/WIP" title="App Komorebi">
           <FaAppStore size={24} color="#fff" />
-        </a>
+        </Link>
       </nav>
     </header>
   );
