0cbe1ab7f273fe0ff6d7b92f049ce441f64b62dc
Fábio Santos <100423829+Gwin14@users.noreply.github.com>
2026-03-20 11:24:17 -0300

    feat: site telemetry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+console.log("Umami ID:", import.meta.env.VITE_UMAMI_WEBSITE_ID);
+if (import.meta.env.VITE_UMAMI_WEBSITE_ID) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = import.meta.env.VITE_UMAMI_SRC;
+  script.setAttribute("data-website-id", import.meta.env.VITE_UMAMI_WEBSITE_ID);
+  document.head.appendChild(script);
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
